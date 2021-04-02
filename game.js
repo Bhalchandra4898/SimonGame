408426a428f367403c761ca84be82ee687d9e42d
BhalchandraLawangare <bhalchandra4898@gmail.com>
2021-04-02 12:35:53 +0530

    Intial changes

---
+++ game.js
@@ -0,0 +1,77 @@
+
+var buttonColours = new Array("red", "blue", "green", "yellow");
+var gamePattern = new Array();
+var userClickedPattern = new Array();
+var level = 0;
+var gameStarted = false;
+
+$(document).keypress(function () {
+  if (!gameStarted) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    gameStarted = true;
+  }
+});
+
+$(".btn").click(function (event) {
+  var userChosenColour = this.id;
+  userClickedPattern.push(userChosenColour);
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+  checkAnswer(userClickedPattern.length-1);
+});
+
+function nextSequence() {
+
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+  level++;
+  $("#level-title").text("Level " + level);
+
+  $("#"+randomChosenColour).fadeOut(100).fadeIn(100)
+  playSound(randomChosenColour);
+
+  userClickedPattern = [];
+}
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
+  setTimeout(function () {
+    $("#" + currentColor).removeClass("pressed");
+  }
+  , 100);
+}
+
+function checkAnswer(currentLevel) {
+  if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(function () {
+        nextSequence();
+      }, 1000)
+    }
+  }
+  else {
+    playSound("wrong");
+    $("body").addClass("game-over");
+    $("#level-title").text("Game over, Press Any Key to Restart");
+
+    setTimeout(function () {
+      $("body").removeClass("game-over");
+    }, 200);
+
+    startOver();
+  }
+}
+
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  gameStarted = false;
+}
